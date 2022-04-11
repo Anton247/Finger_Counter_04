cb9d2ba5d8c2ddf8d824e6cac506a11ced20c32c
Anton <kondratov247@mail.ru>
2022-04-11 16:24:12 +0700

    Final

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import cv2
 import mediapipe as mp
+import time
 
 cap = cv2.VideoCapture(0) # подключение к камере
 mp_Hands = mp.solutions.hands # хотим распознавать руки (hands)
@@ -11,6 +12,7 @@ thumb_Coord = (4, 2) # координаты "суставов" большого 
 
 while cap.isOpened():
     success, image = cap.read()
+    prevTime = time.time()
     if not success:
         print('Не удалось получить кадр с web-камеры')
         continue
@@ -26,20 +28,32 @@ while cap.isOpened():
         upCount = 0 # счётчик пальцев
         for handLms in multiLandMarks:
             mpDraw.draw_landmarks(image, handLms, mp_Hands.HAND_CONNECTIONS)
-            fingersList = [] # список пальцев
-            for idx, lm in enumerate(handLms.landmark):
+            fingersList = [] # список ключевых точек в пикселях
+            for lm in handLms.landmark:
                 h, w, c = image.shape
                 cx, cy = int(lm.x * w), int(lm.y * h)
                 fingersList.append((cx, cy))
+            side = 'left'
+            if fingersList[5][0] > fingersList[17][0]:
+                side = 'right'
+
             for coordinate in finger_Coord:
                 if fingersList[coordinate[0]][1] < fingersList[coordinate[1]][1]:
                     upCount += 1
-            if fingersList[thumb_Coord[0]][0] < fingersList[thumb_Coord[1]][0]:
-                upCount += 1
+            
+            if side == 'left':
+                if fingersList[thumb_Coord[0]][0] < fingersList[thumb_Coord[1]][0]:
+                    upCount += 1
+            else:
+                if fingersList[thumb_Coord[0]][0] > fingersList[thumb_Coord[1]][0]:
+                    upCount += 1
 
         cv2.putText(image, str(upCount), (50, 150), cv2.FONT_HERSHEY_PLAIN, 12, (0, 200, 50), 12)
         print(upCount)
 
+    currentTime = time.time()
+    fps = 1 / (currentTime - prevTime)
+    cv2.putText(image, f'FPS: {int(fps)}', (400, 150), cv2.FONT_HERSHEY_PLAIN, 3, (240, 100, 0), 3)
     cv2.imshow('image', image)
     if cv2.waitKey(1) & 0xFF == 27: # ESC
         break
